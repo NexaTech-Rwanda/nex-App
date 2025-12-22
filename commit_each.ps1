269ed60014130474e3bec408a51f9b69f46ff671
hubby12a <paolauwase555@gmail.com>
2025-12-22 02:10:57 +0200

    Update commit_each.ps1

--- commit_each.ps1
+++ commit_each.ps1
@@ -1,20 +1,14 @@
-# commit_each.ps1
-
 # Get all uncommitted files
 $files = git status --porcelain | ForEach-Object { $_.Substring(3) }
 
 if ($files.Count -eq 0) {
     Write-Host "No uncommitted changes found."
-    exit
+} else {
+    foreach ($file in $files) {
+        Write-Host "Adding and committing file: $file"
+        git add "$file"
+        git commit -m "Update $file"
+        git push
+    }
+    Write-Host "All files committed and pushed individually!"
 }
-
-foreach ($file in $files) {
-    Write-Host "Adding file: $file"
-    git add $file
-
-    # Prompt for commit message
-    $message = Read-Host "Enter commit message for $file"
-    git commit -m $message
-}
-
-Write-Host "All files committed individually!"
