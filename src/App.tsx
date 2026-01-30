8bacdf77a76e6735a5972d1a33649a3c331111fd
hubby12a <paolauwase555@gmail.com>
2026-01-30 20:31:37 +0200

    Update src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import About from "./pages/About";
 import Work from "./pages/Work";
+import ShoppaDetail from "./pages/ShoppaDetail";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
 
@@ -25,6 +26,7 @@ const App = () => (
           {/* Other pages */}
           <Route path="/about" element={<About />} />
           <Route path="/work" element={<Work />} />
+          <Route path="/work/shoppa" element={<ShoppaDetail />} />
           <Route path="/contact" element={<Contact />} />
 
           {/* 404 – MUST stay last */}
